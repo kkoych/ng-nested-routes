de93015bb98b5ba527c3c1d580e6274876e8f7ee
Kalin Koychev <k.koychev@proxiad.com>
2021-08-04 15:02:11 +0300

    Add routes and master component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './first/second/second.component';
+import { ThirdComponent } from './first/second/third/third.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'first',
+    component: FirstComponent,
+    children: [
+      {
+        path: 'second',
+        component: SecondComponent,
+        children: [
+          {
+            path: 'third',
+            component: ThirdComponent,
+            children: [{ path: 'test', component: ThirdComponent }],
+          },
+        ],
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './first/second/second.component';
+import { ThirdComponent } from './first/second/third/third.component';
+import { MasterComponent } from './master/master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    MasterComponent
   ],
   imports: [
     BrowserModule,
